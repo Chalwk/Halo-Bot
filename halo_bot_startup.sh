7258f7a55297e7b82a4241a72af7d2f8ce6c3aee
Chalwk77 <jericho.crosby227@gmail.com>
2024-09-09 13:26:30 +1200

    ...

---
+++ halo_bot_startup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Halo Discord Bot startup script for Linux systems
+
+# Set the JAR file name
+JAR_FILE_NAME="HaloBot-1.1.0.jar"
+
+# Run the bot using Java with specified memory limits
+java -Xms512m -Xmx1024m -jar $JAR_FILE_NAME
+
+# Wait for user input before exiting the script
+read -n 1 -s -r -p "Press any key to continue..."
